37a89e09f56107bd362a9217d3d9906d7d2a6ad7
djvipmax <djvipmax@gmail.com>
2015-10-19 18:03:54 +0300

    added getCredentials function

--- src/main/scala/crawler/TwitterTest.scala
+++ src/main/scala/crawler/TwitterTest.scala
@@ -2,9 +2,9 @@ package crawler
 
 import java.io.File
 
-import twitter4j.conf.ConfigurationBuilder
+import org.slf4j.LoggerFactory
 import twitter4j.{Query, TwitterFactory}
-
+import twitter4j.conf.ConfigurationBuilder
 import scala.collection.JavaConverters._
 import scala.io.Source
 
@@ -12,23 +12,41 @@ import scala.io.Source
  * Created by Max Petrov on 15.10.15.
  */
 object TwitterTest {
+  val logger = LoggerFactory.getLogger("CrawlerScheduler")
+
+  /*
+  * returns Array[(String, String, String, String)] with  twitter credentials
+  * */
+  def getCredentials() = {
+    new File("/Crawler/resources/twitterCreds")
+      .listFiles()
+      .flatMap(file => {
+      Source.fromFile(file)
+        .getLines()
+        .filter(_.length > 10)
+        .grouped(4)
+        .map(group => (group(0).split("key=")(1), group(1).split("secret=")(1), group(2).split("token=")(1), group(3).split("token_secret=")(1)))
+    })
+  }
+
   def main(args: Array[String]) {
     println("Twitter test")
 
-    new File("/Crawler/resources/twitterCreds").listFiles().foreach(file => {println(file); Source.fromFile(file).getLines().foreach(println)})
+    val credentials = getCredentials()
 
-//    val cb = new ConfigurationBuilder()
-//    cb.setDebugEnabled(true)
-//      .setOAuthConsumerKey("WTCtWipd1HPyi0g8WPkyPr6rN")
-//      .setOAuthConsumerSecret("dhGfb2a4b2Ur5MRRe9xFwHxfWJEold3EH3damrw0vocNIVXEJI")
-//      .setOAuthAccessToken("3142277661-rLc76RaBTizQL817WY5AKrKAuJpMjeCEYBUeKma")
-//      .setOAuthAccessTokenSecret("gMvYWDnpNedZ2r9PoEHY9h2eNgOiVFQPenPgZEfXBjtpb")
-//
-//    val twitter = new TwitterFactory(cb.build()).getInstance()
-//
-//    //    val tweets = twitter.getUserTimeline(new Paging(1, 5)).asScala
-//    val tweets = twitter.search(new Query("IGIL").count(100)).getTweets.asScala
-//    tweets.foreach(println)
+
+    val cb = new ConfigurationBuilder()
+    cb.setDebugEnabled(true)
+      .setOAuthConsumerKey(credentials(0)_1)
+      .setOAuthConsumerSecret(credentials(0)_2)
+      .setOAuthAccessToken(credentials(0)_3)
+      .setOAuthAccessTokenSecret(credentials(0)_4)
+
+    val twitter = new TwitterFactory(cb.build()).getInstance()
+
+    //    val tweets = twitter.getUserTimeline(new Paging(1, 5)).asScala
+    val tweets = twitter.search(new Query("IGIL").count(1)).getTweets.asScala
+    tweets.foreach(println)
 
     //    val mongoClient = MongoClient("localhost", 27017)
     //    val db = mongoClient("test")
